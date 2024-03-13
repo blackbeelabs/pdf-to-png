f26caf26a6893af38216f71923706886e4070aee
blackbeelabs <blackbeelabs@outlook.com>
2024-03-13 10:40:54 +0800

    submission

--- src/main.py
+++ src/main.py
@@ -11,35 +11,31 @@ def _get_project_dir_folder():
 def main():
     print("Start: main()")
 
-    extraction_zoom = 2
+    extraction_zoom = 2  # This can take a value between 1 and 10. Refer to https://github.com/GipsyPotter/PDFtoPNG/blob/main/mainpdf.py
+
     ASSETS_FP = path.join(_get_project_dir_folder(), "assets")
 
     downloaded_folder_path = path.join(ASSETS_FP, "downloaded_pdfs")
     preprocessed_folder_path = path.join(ASSETS_FP, "preprocessed_pngs")
-    pdfs = os.listdir(downloaded_folder_path)
-    for pdf in pdfs:
-        files = [
-            f
-            for f in os.listdir(path.join(downloaded_folder_path, y))
-            if ("paper 2" in f.lower() and f.endswith("pdf"))
-        ]
 
-        for i in files:
-            filepath = path.join(downloaded_folder_path, y, i)
-            print(f"INPUT: {filepath}")
-            doc = fitz.open(filepath)
-            j = 0
-            mat = fitz.Matrix(extraction_zoom, extraction_zoom)
-            for page in doc:  # Total number of pages
-                outfilepath = path.join(
-                    preprocessed_folder_path,
-                    f"{y}-{i[:-4]}-pg{j}.png",
-                )
-                print(f"OUTPUT: {outfilepath}")
-                pix = page.get_pixmap(matrix=mat)
-                pix.save(outfilepath)
-                j += 1
-            print()
+    pdfs = [f for f in os.listdir(downloaded_folder_path) if f.endswith(".pdf")]
+    for pdf in pdfs:
+        filepath = path.join(downloaded_folder_path, pdf)
+        print(f"INPUT: {filepath}")
+
+        doc = fitz.open(filepath)
+        mat = fitz.Matrix(extraction_zoom, extraction_zoom)
+        j = 0
+        for page in doc:  # Total number of pages
+            outfilepath = path.join(
+                preprocessed_folder_path,
+                f"{pdf[:-4]}-pg{j}.png",
+            )
+            print(f"OUTPUT: {outfilepath}")
+            pix = page.get_pixmap(matrix=mat)
+            pix.save(outfilepath)
+            j += 1
+        print()
 
     print("Done: main()")
 
